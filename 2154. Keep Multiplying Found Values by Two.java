bc1c324bbb4bbf9b645e707731b524b8236fb3ad
Shruthi S <shruthiofficial29@gmail.com>
2026-07-27 11:29:02 +0530

    Create 2154. Keep Multiplying Found Values by Two.java

---
+++ 2154. Keep Multiplying Found Values by Two.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        for (int num : nums) {
+            if (num == original) {
+                original *= 2;
+            }
+        }
+        return original;
+    }
+}
