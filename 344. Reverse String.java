82b2dba16fa7a2b5972e0677aa0ff0110209e45e
Shruthi S <shruthiofficial29@gmail.com>
2026-07-27 11:13:15 +0530

    Create 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void reverseString(char[] s) {
+        for (int l = 0, r = s.length - 1; l < r; l++, r--) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+        }
+    }
+}
