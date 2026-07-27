7d785e5af43bf7788385d7c068da67f7700e2a3a
Shruthi S <shruthiofficial29@gmail.com>
2026-07-27 11:16:30 +0530

    Create 1221. Split a String in Balanced Strings.java

---
+++ 1221. Split a String in Balanced Strings.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int res = 0, count = 0;
+        for (char c : s.toCharArray()) {
+            count += (c == 'R') ? 1 : -1;
+            if (count == 0) res++;
+        }
+        return res;
+    }
+}
