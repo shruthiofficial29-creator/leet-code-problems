ecf55b4f4abf779c9862c6aaa0dfae924ebb5003
Shruthi S <shruthiofficial29@gmail.com>
2026-07-27 11:30:15 +0530

    Create 2180. Count Integers With Even Digit Sum.java

---
+++ 2180. Count Integers With Even Digit Sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countEven(int num) {
+        int temp = num, sum = 0;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return (sum % 2 == 0) ? num / 2 : (num - 1) / 2;
+    }
+}
